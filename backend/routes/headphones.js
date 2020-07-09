0f4677306a466d3a3eaab751345a2867de2670d3
chiay <chiay@oregonstate.edu>
2020-07-09 00:19:31 -0700

    done get all route

--- backend/routes/headphones.js
+++ backend/routes/headphones.js
@@ -2,5 +2,19 @@ const express = require('express');
 const router = express.Router();
 const Headphone = require("../models/headphone");
 
+// Get all headphones
+
+router.get('/', async (req, res) => {
+   try {
+      const headphones = await Headphone.find({});
+      res.json(headphones);
+   } catch (err) {
+      res.statusCode(500).json({ msg: err.message });
+   }
+});
+
+// Update one headphone
+
+// Delete one headphone
 
 module.exports = router;
